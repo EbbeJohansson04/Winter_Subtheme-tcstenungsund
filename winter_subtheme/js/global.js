9b13bc012354b83fe96e3790771b65496ec61b76
Ebbe Johanssson <113424384+EbbeJohansson04@users.noreply.github.com>
2024-05-07 10:51:00 +0200

    Add files via upload
    Försökte använda mig av scss för en del av hemsidan men tror inte det fungerade så ignorera den filen

---
+++ winter_subtheme/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.winter_subtheme = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
